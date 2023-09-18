ea72094025601d41a83bc0a3f43b569bff727a96
Mohammad Masood Alam <130masoodalam@gmail.com>
2023-09-18 16:27:06 +0530

    please recheck insertatbeginning.c

---
+++ insertatbeginning.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "node.h"
+
+node *insertAtBeginning(node *header)
+{
+  int n; // number of nodes to be inserted.
+  traversal(header);
+  printf("Enter the number of nodes to be inserted: ");
+  scanf("%d", &n);
+  while (n != 0)
+  {
+    node *new = (node *)malloc(sizeof(node));
+    if (new == NULL)
+    {
+      printf("Memory insufficient...\n\n");
+      return header;
+    }
+    printf("Enter the data for the new node: ");
+    scanf("%d", &new->data);
+    new->link = header->link;
+    header->link = &new;
+    printf("%x\n", header->link);
+    n--;
+  }
+  traversal(header);
+  return header;
+}
