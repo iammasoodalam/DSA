ffd112f23c612b43e4f142566ca952629a3e19ab
Mohammad Masood Alam <130masoodalam@gmail.com>
2023-09-19 18:54:04 +0530

    now you can delete the first node of doubly linked list

---
+++ Doubly Linked List/deleteatbeginning.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "doublynode.h"
+
+doublynode *deleteAtBeginning(doublynode *header)
+{
+  doublynode *p = header->next, *pFront;
+  pFront = p->next;
+  header->next = pFront;
+  pFront->previous = header;
+  free(p);
+  doublyTraversal(header);
+  return header;
+}
