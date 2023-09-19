f4053f7bafb22cd3b171e5e13bbc2c3b94f847f0
Mohammad Masood Alam <130masoodalam@gmail.com>
2023-09-20 03:53:33 +0530

    insert desired number of nodes at the end of a linked list

---
+++ Linked List/insertatend.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "node.h"
+
+node *insertAtEnd(node *header)
+{
+  node *ptr;
+  int n;             // number of nodes to be inserted
+  traversal(header); // logging the list before insertion
+  printf("Enter the number of nodes to be inserted: ");
+  scanf("%d", &n);
+
+  // setting a pointer at the end of the linked list.
+  ptr = header->link;
+  while (ptr->link != NULL)
+  {
+    ptr = ptr->link;
+  }
+
+  while (n != 0)
+  {
+    node *new = (node *)malloc(sizeof(node));
+    if (new == NULL)
+    {
+      printf("Memory insufficient...\n\n");
+      return header;
+    }
+    printf("Enter the data for the new node: ");
+    scanf("%d", &new->data);
+    new->link = NULL;
+    ptr->link = new;
+    ptr = new;
+    n--;
+  }
+  traversal(header);
+  return header;
+}
