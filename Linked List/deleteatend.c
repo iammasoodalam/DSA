52a527ab5c10ab55bbd4e59b5610fa15b0f60591
Mohammad Masood Alam <130masoodalam@gmail.com>
2023-09-20 05:09:21 +0530

    it will delete the last node and return a header pointer, by which you can traversal(*youlist)

---
+++ Linked List/deleteatend.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "node.h"
+
+node *deleteAtEnd(node *header)
+{
+  node *ptr = header->link;
+
+  // check if link is empty or not.
+  if (ptr == NULL)
+  {
+    printf("List is already empty...\n Delete operation is not possible.");
+    return header;
+  }
+  node *ptr1 = ptr->link;
+
+  while (ptr1->link != NULL)
+  {
+    ptr = ptr1;
+    ptr1 = ptr1->link;
+  }
+  ptr->link = NULL;
+  free(ptr1);
+
+  return header;
+}
