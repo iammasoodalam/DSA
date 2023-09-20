6854a16fcc37e9ef1bbe405fbd4b5826e94360d4
Mohammad Masood Alam <130masoodalam@gmail.com>
2023-09-20 22:39:59 +0530

    this function will return the address of the searched node

---
+++ Linked List/searchanode.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "node.h"
+
+node *searchANode(node *header)
+{
+  int found = 0, position = 0, key;
+  node *location;
+  node *ptr = header->link;
+
+  printf("Enter the number you want to search in the list: ");
+  scanf("%d", &key);
+
+  while ((ptr != NULL) && (found == 0))
+  {
+    position++;
+    if (ptr->data == key)
+    {
+      found = 1;
+      location = ptr;
+    }
+    else
+    {
+      ptr = ptr->link;
+    }
+  }
+  if (found == 1)
+  {
+    printf("%d is found in %d position at address of %p\n", key, position, location);
+  }
+  else
+  {
+    printf("The number %d is not available.", key);
+  }
+  return location;
+}
