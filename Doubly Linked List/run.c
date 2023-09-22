16865d20267d0ea2f3b2bd8446afbe8aaaf39c36
Mohammad Masood Alam <130masoodalam@gmail.com>
2023-09-22 10:46:15 +0530

    Added switch case for operation, there is problem in creating a list in, line: 44

--- Doubly Linked List/run.c
+++ Doubly Linked List/run.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h> // for using toupper()... this will change a character to uppercase.
 #include "doublynode.h"
 #include "createdoubly.c"
 #include "traversaldoubly.c"
@@ -10,9 +11,88 @@
 #include "deleteatbeginning.c"
 #include "deleteatanypos.c"
 
-void main()
+int main()
 {
+  int operation; // operation type
+  char cont; // continue
 
-  doublynode *list1 = createDoubly();
-  deleteAtAnyPosition(list1);
+  RESTART:
+  printf("1. Create a list.\n");
+  printf("2. Insert at beginning of a doubly linked list.\n");
+  printf("3. Insert at the end of a doubly linked list.\n");
+  printf("4. Insert at any position of a doubly linked list.\n");
+  printf("5. Delete at the beginning of a doubly linked list.\n");
+  printf("6. Delete at the end of a doubly linked list.\n");
+  printf("7. Delete at any position of a doubly linked list.\n");
+  printf("8. Calculate the node count of a doubly linked list.\n");
+
+  CHOOSE_OPERATION:
+  printf("Choose any operation provided above: ");
+  scanf("%d", &operation);
+
+  doublynode *list;
+  if (operation != 1)
+  {
+    printf("\nCreate your list before doing an operation...\n");
+    list = createDoubly();
+    doublyTraversal(list);
+  }
+
+  switch (operation)
+  {
+  case 1:
+    doublynode *list1 = createDoubly();
+    doublyTraversal(list1);
+    break;
+  case 2:
+    list = insertAtBeginning(list);
+    doublyTraversal(list);
+    break;
+  case 3:
+    list = insertAtEnd(list);
+    doublyTraversal(list);
+    break;
+  case 4:
+    list = insertAtAnyPosition(list);
+    doublyTraversal(list);
+    break;
+  case 5:
+    list = deleteAtBeginning(list);
+    doublyTraversal(list);
+    break;
+  case 6:
+    list = deleteAtEnd(list);
+    doublyTraversal(list);
+    break;
+  case 7:
+    list = deleteAtAnyPosition(list);
+    doublyTraversal(list);
+    break;
+  default:
+    printf("Please enter a valid value..\n ");
+    goto CHOOSE_OPERATION;
+    break;
+  }
+
+  ASK_REDO:
+  printf("Do you want to continue...\n");
+  printf("Enter Y for yes and N for No: ");
+  getchar();
+  scanf("%c", &cont);
+  cont = (char)toupper(cont);
+
+  if (cont == 89) // 89 is equivalent to Y
+  {
+    goto RESTART;
+  }
+  else if (cont == 78) // 78 is equivalent to N
+  {
+    return 0;
+  }
+  else
+  {
+    printf("Please read carefully...\n\n");
+    goto ASK_REDO;
+  }
+  return 0;
 }
